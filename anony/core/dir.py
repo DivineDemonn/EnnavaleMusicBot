4c47155b98fe48653e6175e60f1e8edcb12ea241
Divine Demon <shadowloard04@gmail.com>
2026-05-26 23:03:31 +0530

    Update dir.py

--- anony/core/dir.py
+++ anony/core/dir.py
@@ -2,11 +2,13 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-
 import shutil
 from pathlib import Path
 
 from anony import logger
+
+CACHE_DIR = "cache"
+DOWNLOADS_DIR = "downloads"
 
 
 def ensure_dirs():
@@ -16,6 +18,7 @@ def ensure_dirs():
     if not shutil.which("deno") or not shutil.which("ffmpeg"):
         raise RuntimeError("Deno and FFmpeg must be installed and accessible in the system PATH.")
 
-    for dir in ["cache", "downloads"]:
+    for dir in [CACHE_DIR, DOWNLOADS_DIR]:
         Path(dir).mkdir(parents=True, exist_ok=True)
+
     logger.info("Cache directories updated.")
